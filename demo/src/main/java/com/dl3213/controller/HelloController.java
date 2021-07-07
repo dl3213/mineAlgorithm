462759d726966f51aa5cd1fc8d536f6a6ada8b8b
dl3213 <dl3213@192.168.0.101>
2021-07-07 22:21:46 +0800

    7.7 main test

---
+++ demo/src/main/java/com/dl3213/controller/HelloController.java
@@ -0,0 +1,16 @@
+
+package com.dl3213.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/hello")
+    public String say(){
+        return "msg";
+    }
+}
---
+++ demo/src/test/java/com/dl3213/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.dl3213.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void test01() {
+		System.out.println("test01");
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
